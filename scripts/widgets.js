fec9ff7ea95c440b1712c3d50abb937867c1e768
ericgold <egold05@gmail.com>
2016-07-01 17:06:40 -0700

    global chart configurations

---
+++ scripts/widgets.js
@@ -0,0 +1,9 @@
+// GLOBAL CHART.JS CONFIGURATIONS
+
+Chart.defaults.global.defaultFontColor = 'gray';
+Chart.defaults.global.defaultFontFamily = 'HelveticaNeue';
+Chart.defaults.global.defaultFontSize = 12;
+Chart.defaults.global.defaultFontStyle = 'normal';
+Chart.defaults.global.responsive = true;
+Chart.defaults.global.responsiveAnimationDuration = 0;
+Chart.defaults.global.maintainAspectRatio = true;
