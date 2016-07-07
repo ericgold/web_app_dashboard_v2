cf4595eed5bdc33da15c934e77c07a7882775a15
ericgold <egold05@gmail.com>
2016-07-07 15:57:26 -0700

    doughnut chart setup
    added basic doughnut chart with data and options

--- scripts/widgets.js
+++ scripts/widgets.js
@@ -8,3 +8,39 @@ Chart.defaults.global.responsive = true;
 Chart.defaults.global.responsiveAnimationDuration = 0;
 Chart.defaults.global.maintainAspectRatio = true;
 Chart.defaults.global.legend.display = false;
+
+
+// Mobile User Doughnut Chart
+
+var mobileUsers = document.getElementById('mobile-users');
+
+var mobileUsersData = {
+	labels: [
+		"Phones",
+		"Tablets",
+		"Desktop"
+	],
+	datasets: [
+		{
+			data: [150, 70, 50],
+			backgroundColor: [
+				"#7379BD",
+				"#83C891",
+				"#76B1BE"
+			]
+		}
+	]
+};
+
+var mobileUsersOptions = {
+	legend: {
+		display: true,
+		position: 'bottom'
+	}
+};
+
+var mobileUserChart = new Chart(mobileUsers, {
+	type: 'doughnut',
+	data: mobileUsersData,
+	options: mobileUsersOptions
+});
