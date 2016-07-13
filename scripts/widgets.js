f034e9a09f0e438849a85814306c23c7b62bf0e2
ericgold <egold05@gmail.com>
2016-07-12 19:26:31 -0700

    added alert cancel
    added variables, function, and event listener to cancel the alert
    
    added provisional styles to add the alert notification dot next to the
    bell icon

--- scripts/widgets.js
+++ scripts/widgets.js
@@ -8,3 +8,15 @@ Chart.defaults.global.responsive = true;
 Chart.defaults.global.responsiveAnimationDuration = 0;
 Chart.defaults.global.maintainAspectRatio = true;
 Chart.defaults.global.legend.display = false;
+
+
+// Alert Cancel
+
+var alertBar = document.getElementById('alert');
+var alertX = document.getElementById('alert-x');
+
+function hideAlert() {
+	alertBar.style.display = "none";
+}
+
+alertX.addEventListener("click", hideAlert);
