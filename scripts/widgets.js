03eb2f9e7294b9de548bce77dec7bc2d7b036867
ericgold <egold05@gmail.com>
2016-07-20 18:18:55 -0700

    fixed chart updates
    chart now updates to new data, labels, y-axis step size and y-axis max
    for each traffic line nav button
    
    extended %widget-spacing in the chart canvas itself

--- scripts/widgets.js
+++ scripts/widgets.js
@@ -36,24 +36,58 @@ function toggleTrafficActive(btn) {
 
 // Traffic Line Chart
 
-
 // Traffic Line Data
 
-var trafficLineDataHourly = {
-	labels: [
-			 "12am",
-			 "2am",
-			 "4am", 
-			 "6am", 
-			 "8am", 
-			 "10am", 
-			 "12pm", 
-			 "2pm", 
-			 "4pm", 
-			 "6pm",
-			 "8pm", 
-			 "10pm"
-	],
+var hourlyData = [0, 2, 5, 7, 10, 12, 16, 18, 15, 14, 13, 8, 4];
+var dailyData = [110, 120, 325, 410, 80, 70, 130, 196, 120, 278, 400, 110, 143];
+var weeklyData = [0, 500, 1000, 750, 1250, 1750, 1250, 1500, 1000, 1500, 2000, 1500, 2000];
+var monthlyData = [0, 1900, 3500, 3000, 4500, 4800, 5000, 4700, 3900, 2600, 2050, 2100, 3700];
+
+var hourlyStepSize = 5;
+var hourlyMax = 20;
+
+var dailyStepSize = 100;
+var dailyMax = 500; 
+
+var weeklyStepSize = 500;
+var weeklyMax = 2500
+
+var monthlyStepSize = 2000;
+var monthlyMax = 10000;
+
+var hourlyLabels = ["12am",
+					 "2am",
+					 "4am", 
+					 "6am", 
+					 "8am", 
+					 "10am", 
+					 "12pm", 
+					 "2pm", 
+					 "4pm", 
+					 "6pm",
+					 "8pm", 
+					 "10pm"
+					];
+var dailyLabels = ["7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18"];
+var weeklyLabels = [ "0",
+					 "16-22",
+					 "23-29", 
+					 "30-5", 
+					 "6-12", 
+					 "13-19", 
+					 "20-26", 
+					 "27-3", 
+					 "4-10", 
+					 "11-17",
+					 "19-24", 
+					 "25-31"
+					];
+var monthlyLabels = ["J", "A", "S", "O", "N", "D", "J", "F", "M", "A", "M", "J"];
+
+//default data for the traffic line chart, including weekly defaults
+var trafficLineData = {
+
+	labels: weeklyLabels,
 
 	datasets: [
 		{
@@ -70,124 +104,13 @@ var trafficLineDataHourly = {
 			pointHitRadius: 10,
 			pointStyle: 'circle',
 			showLines: true,	
-			data: [0, 10, 10, 20, 15, 80, 120, 200, 150, 250, 180, 100, 60]
+			data: weeklyData
 		}
 	]
 };
 
-var trafficLineDataDaily = {
-	labels: [
-			 "0",
-			 "16-22",
-			 "23-29", 
-			 "30-5", 
-			 "6-12", 
-			 "13-19", 
-			 "20-26", 
-			 "27-3", 
-			 "4-10", 
-			 "11-17",
-			 "19-24", 
-			 "25-31"
-	],
-
-	datasets: [
-		{
-			fill: true,
-			lineTension: 0,
-			backgroundColor: '#E2E3F5',
-			pointBackgroundColor: '#FFFFFF',
-			borderWidth: 1,
-			borderColor: '#7379BD',
-			pointBorderColor: '#7379BD',
-			pointBorderWidth: 2,
-			pointRadius: 4,
-			pointHoverRadius: 10,
-			pointHitRadius: 10,
-			pointStyle: 'circle',
-			showLines: true,	
-			data: [0, 500, 1000, 750, 1250, 1750, 1250, 1500, 1000, 1500, 2000, 1500, 2000]
-		}
-	]
-};
-
-var trafficLineDataWeekly = {
-
-	labels: [
-			 "0",
-			 "16-22",
-			 "23-29", 
-			 "30-5", 
-			 "6-12", 
-			 "13-19", 
-			 "20-26", 
-			 "27-3", 
-			 "4-10", 
-			 "11-17",
-			 "19-24", 
-			 "25-31"
-	],
-
-	datasets: [
-		{
-			fill: true,
-			lineTension: 0,
-			backgroundColor: '#E2E3F5',
-			pointBackgroundColor: '#FFFFFF',
-			borderWidth: 1,
-			borderColor: '#7379BD',
-			pointBorderColor: '#7379BD',
-			pointBorderWidth: 2,
-			pointRadius: 4,
-			pointHoverRadius: 10,
-			pointHitRadius: 10,
-			pointStyle: 'circle',
-			showLines: true,	
-			data: [0, 500, 1000, 750, 1250, 1750, 1250, 1500, 1000, 1500, 2000, 1500, 2000]
-		}
-	]
-};
-
-var trafficLineDataMonthly = {
-	labels: [
-			 "0",
-			 "16-22",
-			 "23-29", 
-			 "30-5", 
-			 "6-12", 
-			 "13-19", 
-			 "20-26", 
-			 "27-3", 
-			 "4-10", 
-			 "11-17",
-			 "19-24", 
-			 "25-31"
-	],
-
-	datasets: [
-		{
-			fill: true,
-			lineTension: 0,
-			backgroundColor: '#E2E3F5',
-			pointBackgroundColor: '#FFFFFF',
-			borderWidth: 1,
-			borderColor: '#7379BD',
-			pointBorderColor: '#7379BD',
-			pointBorderWidth: 2,
-			pointRadius: 4,
-			pointHoverRadius: 10,
-			pointHitRadius: 10,
-			pointStyle: 'circle',
-			showLines: true,	
-			data: [0, 500, 1000, 750, 1250, 1750, 1250, 1500, 1000, 1500, 2000, 1500, 2000]
-		}
-	]
-};
-
-
-// Traffic Line Options
-
-var trafficLineOptionsHourly = {
+//default options for the traffic line chart, including weekly defaults
+var trafficLineOptions = {
 	scales: {
 		
 		xAxes: [{
@@ -202,35 +125,8 @@ var trafficLineOptionsHourly = {
 		yAxes: [{
 			ticks: {
 				display: true,
-				stepSize: 50,
-				max: 250,		
-			},
-			gridLines: {
-				drawTicks: false,
-			}		
-		}]
-	}
-};
-
-var trafficLineOptionsDaily = {};
-
-var trafficLineOptionsWeekly = {
-	scales: {
-		
-		xAxes: [{
-			ticks: {
-				display: true			
-			},
-			gridLines: {
-				drawTicks: false
-			}	
-		}],
-
-		yAxes: [{
-			ticks: {
-				display: true,
-				stepSize: 500,
-				max: 2500,		
+				stepSize: weeklyStepSize,
+				max: weeklyMax,		
 			},
 			gridLines: {
 				drawTicks: false,
@@ -239,51 +135,54 @@ var trafficLineOptionsWeekly = {
 	}	
 };
 
-var trafficLineOptionsMonthly = {};
-
 // Create the traffic line chart with weekly data and options
-
 var trafficLine = new Chart(trafficLineWidget, {
 	type: 'line',
-	data: trafficLineDataWeekly,
-	options: trafficLineOptionsWeekly
+	data: trafficLineData,
+	options: trafficLineOptions
 });
 
 // Functions and event listeners for refreshing the chart
-
 function hourlyTraffic() {
-
 	//highlight Hourly button
 	toggleTrafficActive(hourlyButton);
-
-	//swap data object in trafficLine chart (doesn't work)
-	trafficLine.data = trafficLineDataHourly;
-	//console.log(trafficLine.data);
-	
-	//swap options object in trafficLine chart (does work?)
-	trafficLine.options = trafficLineOptionsHourly;
-	//console.log(trafficLine.options);
-	
+	//set data array in trafficLine chart 
+	trafficLine.data.datasets[0].data = hourlyData;
+	//set labels as hourly labels
+	trafficLine.data.labels = hourlyLabels;
+	//set y-axis step size and max
+	trafficLine.options.scales.yAxes[0].ticks.stepSize = hourlyStepSize;
+	trafficLine.options.scales.yAxes[0].ticks.max = hourlyMax;
 	//redraw chart with new data
 	trafficLine.update();
 }
 
 function dailyTraffic() {
 	toggleTrafficActive(dailyButton);
-	trafficLine.data = trafficLineDataDaily;
+	trafficLine.data.datasets[0].data = dailyData;
+	trafficLine.data.labels = dailyLabels;
+	trafficLine.options.scales.yAxes[0].ticks.stepSize = dailyStepSize;
+	trafficLine.options.scales.yAxes[0].ticks.max = dailyMax;
+	trafficLine.update();
 }
 
 function weeklyTraffic() {
 	toggleTrafficActive(weeklyButton);
-	trafficLine.data = trafficLineDataWeekly;
+	trafficLine.data.datasets[0].data = weeklyData;
+	trafficLine.data.labels = weeklyLabels;
+	trafficLine.options.scales.yAxes[0].ticks.stepSize = weeklyStepSize;
+	trafficLine.options.scales.yAxes[0].ticks.max = weeklyMax;
+	trafficLine.update();
 }
 
 function monthlyTraffic() {
 	toggleTrafficActive(monthlyButton);
-	trafficLine.data = trafficLineDataMonthly;
+	trafficLine.data.datasets[0].data = monthlyData;
+	trafficLine.data.labels = monthlyLabels;
+	trafficLine.options.scales.yAxes[0].ticks.stepSize = monthlyStepSize;
+	trafficLine.options.scales.yAxes[0].ticks.max = monthlyMax;
+	trafficLine.update();
 }
-
-
 
 // Traffic Line Chart navigation event listeners
 
