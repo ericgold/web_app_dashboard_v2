765cb070e79a3d21047d4f6effaf2d1499b43c39
ericgold <egold05@gmail.com>
2016-07-24 18:36:39 -0700

    search suggestions
    added code to provide user suggestions based on search query, but it’s
    buggy
    
    added event listener and function to populate search input when one of
    the suggestions is clicked

--- scripts/widgets.js
+++ scripts/widgets.js
@@ -19,10 +19,14 @@ var messageButton = document.getElementById("message-button");
 
 //variables for message user search dropdown 
 var userDiv = document.createElement("div");
-var userBox = document.createElement("div");
+
+var userBoxes = document.getElementsByClassName("user-box");
 
 //array to hold users
 var users = [];
+
+//array to hold users that match the search
+var matchedUsers = [];
 
 //constructor function for users
 function User(firstName, lastName, email, joinDate) {
@@ -46,10 +50,13 @@ var user4 = new User('Alice', 'Inwonderland', 'alice@rabbithole.com', '7/13/16')
 var messageDiv = document.createElement("div");
 //container for an individual error or confirmation message
 var messageBox = document.createElement("div");
+
+//error and confirmation messages
 var userError = "Please choose a user to message";
 var messageError = "Please enter a message";
 var sendConfirm = "Message sent";
 
+//* functions for error and confirmation messages *
 function setupMessageDiv() {
 	messageDiv.setAttribute("id", "message-div");
 }
@@ -64,79 +71,96 @@ function showMesssageDiv() {
 	messageUserWidget.insertBefore(messageDiv, userInput);
 }
 
-function showResults() {
-	//add matched search results' first and last names
-	//to a userBox
-	//add that userBox to the userDiv
-}
+//* functions for user search and suggestions *
 
-/*
-function userSearch() {
-	//filters the input so only matched users display
-	var query = userInput.value;
-	//set variable for contents of users first name and last name
-	
-	//compare query to that variable
-
-	//if query is in the array (!== -1) 
-
-		//add that user's name to the dropdown
-	
-	for (var i = 0; i < users.length; i++) {
-		if (users[i].firstName === query || users[i].lastName === query) {
-			console.log(users[i].firstName + " " + users[i].lastName)
-		}
-	}	
-}
-*/
-
+//create a div to hold the list of suggestions
 function setupUserDiv() {
 	userDiv.setAttribute("id", "user-div");
 }
 
+//create divs to hold individual suggestions
 function setupUserBox(suggestion) {
+	var userBox = document.createElement("div");
 	userBox.classList.add("user-box");
 	userBox.textContent = suggestion.firstName + " " + suggestion.lastName;
 	userDiv.appendChild(userBox);
-	
 }
 
+
+//display the list of suggested users based on search term
 function showUserDiv() {
 	messageUserWidget.insertBefore(userDiv, userMessage);
 }
 
+//remove user suggestions
 function removeUserDiv() {
+	clearUserDiv();
 	messageUserWidget.removeChild(userDiv);
+	matchedUsers = [];
+	matchedUsers.length = 0;
 }
 
+function clearUserDiv() {
+	for (i = 0; i < userDiv.childNodes.length; i++) {
+		userDiv.removeChild[i]
+	}	
+}
+
+// enter selected suggestion into input field and remove suggestions
 function userSelect() {
 	userInput.value = this.textContent.toString();
 	removeUserDiv();
 }
 
+function activateSelect() {
+	for (var i = 0; i < userBoxes.length; i++) {
+		userBoxes[i].addEventListener('click', userSelect);
+	}
+}
+
+function matchUsers(arg) {
+	
+	//check each user in users
+	for (var i = 0; i < users.length; i++) {
+			var firstIndex = users[i].firstName.toLowerCase().indexOf(arg);
+			var lastIndex = users[i].lastName.toLowerCase().indexOf(arg);
+			//if the query is present in first name or last name of that user
+			if (firstIndex !== -1 || lastIndex !== -1) {
+				//add that user to matchedUsers
+				matchedUsers.push(users[i]);
+				
+			//otherwise	
+			} else {
+				//remove that user from matchedUsers
+				matchedUsers.pop(users[i]);
+			}
+		}
+}
+
+function addMatchedUsers() {
+	//for each user in matchedUsers
+	for (var i = 0; i < matchedUsers.length; i++) {
+			//set up a userBox and add it to the userDiv
+			setupUserBox(matchedUsers[i]);
+		}
+}
 
 function userSearch() {
 	
-	var query = userInput.value;
-
-	setupUserDiv();
-
+	var query = userInput.value.toLowerCase();
+	
+	//if there is a query
 	if(query) {
-		for (var i = 0; i < users.length; i++) {
-			if (users[i].firstName.indexOf(query) !== -1) {
-				//console.log(users[i].firstName + " " + users[i].lastName);
-				
-				setupUserBox(users[i]);
-			}
-			else if (users[i].lastName.indexOf(query) !== -1) {
-				//console.log(users[i].firstName + " " + users[i].lastName);
-				
-				setupUserBox(users[i]);
-			}
-		}
-
+		//check for users matching the query
+		matchUsers(query);
+		//create container for user suggestions
+		setupUserDiv();
+		//add each matched users to a userBox
+		addMatchedUsers();
+		//show the populated userDiv
 		showUserDiv();	
 	} else {
+		//if there is no query, remove the userDiv
 		removeUserDiv();
 	}
 }
@@ -192,4 +216,5 @@ function hideMessage() {
 messageButton.addEventListener("click", messageCheck);
 userInput.addEventListener("keyup", userSearch);
 messageDiv.addEventListener("click", hideMessage);
-userBox.addEventListener("click", userSelect);
+
+userDiv.addEventListener("mouseover", activateSelect);
