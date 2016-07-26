132e839d45b11af6897cb33792e67a23891c567d
ericgold <egold05@gmail.com>
2016-07-25 19:03:37 -0700

    commit merge traffic-daily

--- scripts/widgets.js
+++ scripts/widgets.js
@@ -9,6 +9,72 @@ Chart.defaults.global.responsiveAnimationDuration = 0;
 Chart.defaults.global.maintainAspectRatio = true;
 Chart.defaults.global.legend.display = false;
 
+
+// Daily Traffic Widget (Bar Chart)
+
+var trafficDaily = document.getElementById('traffic-bar');
+
+var trafficDailyData = {
+		labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+		datasets: [ 
+			{
+				label: 'Desktop',
+				backgroundColor: '#7379BD',
+				data: [75, 100, 175, 125, 225, 200, 100],
+			},
+
+			{
+				label: 'Tablets',
+				backgroundColor: '#83C891',
+				data: [60, 80, 120, 70, 180, 150, 70],
+			},
+			
+			{
+				label: 'Phones',
+				backgroundColor: '#76B1BE',
+				data: [80, 20, 40, 80, 80, 70, 90],
+			}
+
+		]
+}
+
+var trafficDailyOptions = {
+	legend: {
+		display: true,
+		position: 'bottom',
+	},
+		
+	
+	scales: {
+		xAxes: [{
+			categoryPercentage: 0.8,
+			barPercentage: 0.8,
+			gridLines: {
+				offsetGridLines: true,
+				drawTicks: false
+			}
+		}],
+		yAxes: [{
+			type: 'linear',
+			gridLines: {
+				offsetGridLines: true,
+				drawTicks: false
+			},
+			ticks: {
+				beginAtZero: false,
+				stepSize: 50,
+			}
+		}]
+	}
+}
+
+var trafficDailyChart = new Chart(trafficDaily, {
+	type: 'bar',
+	data: trafficDailyData,
+	options: trafficDailyOptions
+});
+
+
 // Main Navigation
 var navButton = document.getElementsByClassName("nav-icon");
 var dashboardIcon = document.getElementById("dashboard-icon");
@@ -308,3 +374,4 @@ function hideAlert() {
 
 //event listener for alert bar x
 alertX.addEventListener("click", hideAlert);
+
